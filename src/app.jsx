e48ad529812f9036747cbd337b7178c68f5b6a09
daniel0426 <angus9209@gmail.com>
2021-08-31 09:24:15 +1200

    get data from api

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import VideoList from './components/video_list/video_list';
 
-function app() {
-  return (
-    <div>
-      
-    </div>
-  )
+function App() {
+  const [videos, setVideos]= useState([]);
+
+  useEffect(()=> {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDPpl4M-Fr1wKscqvrKXgcG8xZHWeV1-Ok", 
+      requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[])
+  return <VideoList videos={videos}/>
+  
 }
 
-export default app
+export default App
