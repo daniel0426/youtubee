f31a9bc64238b431d6a9f0df1f85227157145d56
daniel0426 <angus9209@gmail.com>
2021-08-31 09:07:15 +1200

    refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import './app.css';
-import SearchBar from './components/searchBar';
-import VideoList from './components/video_list/videoList';
+import React from 'react'
 
-function App() {
-  const [videos, setVideos] = useState([]);
-  const [videoChannel, setVideoChannel] = useState([]);
-
- useEffect(()=> {
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDPpl4M-Fr1wKscqvrKXgcG8xZHWeV1-Ok", requestOptions)
-    .then(res => res.json())
-    .then(data => setVideos(data.items))
-    .catch(error => console.log('error', error));
- },[])
-
- 
-
+function app() {
   return (
     <div>
-    <SearchBar/>
-    <VideoList videos={videos}/>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default app
