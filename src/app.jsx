cb937e7013f6b16a2d5185d2e195c87845e4fab9
daniel0426 <angus9209@gmail.com>
2021-08-31 11:04:58 +1200

    network.js made to handle data transaction

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import VideoList from './components/video_list/video_list';
 import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({network}) {
   const [videos, setVideos]= useState([]);
 
-  useEffect(()=> {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDPpl4M-Fr1wKscqvrKXgcG8xZHWeV1-Ok", 
-      requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  },[])
-  return <VideoList videos={videos}/>
+  const search = (query)=> {
+    network
+    .search(query)
+    .then(videos=> setVideos(videos))
+    .catch(error => console.log('error', error));
   
+  }
+  
+  useEffect(()=> {
+      network
+      .mostPopular()
+      .then(videos=> setVideos(videos))
+      .catch(error => console.log('error', error));
+  },[network]);
+
+  return (
+    <div className = {styles.app}>                
+      <SearchHeader onSearch = {search}/>         
+      <VideoList videos={videos}/>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import Network from './service/network';
+
+const network = new Network(process.env.REACT_APP_NETWORK_API_KEY)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App network={network}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
